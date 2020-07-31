c5fb9d91036a330c7c6da6a1d0bea543963359be
CherryPetale <pc1247@pishi1247noMacBook-Pro.local>
2020-07-31 09:49:42 +0900

    ミス修正

--- src/Point.java
+++ src/Point.java
@@ -2,16 +2,16 @@ public class Point{
     private double x;
     private double y;
 
-    public void Point(double x, double y){
+    Point(double x, double y){
         this.x = x;
         this.y = y;
     }
 
-    public getX(){
+    public double getX(){
         return this.x;
     }
 
-    public getY(){
+    public double getY(){
         return this.y;
     }
 }
