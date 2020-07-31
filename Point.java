d08614734a7b2d8390ff302a6c6629959df44355
CherryPetale <pc1247@pishi1247noMacBook-Pro.local>
2020-07-31 09:16:25 +0900

    #1 形だけ

---
+++ Point.java
@@ -0,0 +1,17 @@
+public class Point{
+    private double x;
+    private double y;
+
+    public void Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public getX(){
+        return this.x;
+    }
+
+    public getY(){
+        return this.y;
+    }
+}
